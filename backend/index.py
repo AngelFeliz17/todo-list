775d72bb062775ae748244087497d3402cbf174f
Angel Feliz <52576090+AngelFeliz17@users.noreply.github.com>
2025-09-17 11:47:44 -0500

    added backend folder

---
+++ backend/index.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, HTTPException, Body
+from pydantic import BaseModel
+from typing import Optional
+
+from setting_db import create_db_and_tables, Tasks, SessionDep
+from sqlmodel import select
+
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+@app.get("/tasks/")
+def readTasks(session: SessionDep) -> list[Tasks]:
+    tasks = session.exec(select(Tasks)).all()
+    return tasks
+
+@app.post("/tasks")
+def create_task(task: Tasks, session: SessionDep):
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
+
+@app.get("/tasks/{task_id}", response_model=Tasks)
+def readTaskByID(task_id: int, session: SessionDep):
+    task = session.get(Tasks, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+    return task
+
+@app.delete("/tasks/{task_id}")
+def deleteTask(task_id: int, session: SessionDep):
+    task = session.get(Tasks, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+    session.delete(task)
+    session.commit()
+    return {"msg": f"'{task.task}' has been deleted."}
+
+@app.put("/tasks/{task_id}", response_model=Tasks)
+def updateTask(task_id: int, session: SessionDep, task_update: dict = Body(...)):
+    task = session.get(Tasks, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+    
+    if "task" in task_update:
+        task.task = task_update["task"]
+    if "is_done" in task_update:
+        task.is_done = task_update["is_done"]
+    if "pic" in task_update:
+        task.pic = task_update["pic"]
+
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
---
+++ backend/setting_db.py
@@ -0,0 +1,25 @@
+from typing import Annotated, Optional
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine
+
+
+class Tasks(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    task: str | None = Field(index=True)
+    pic: Optional[str] = None
+    is_done: bool = False
+    
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
